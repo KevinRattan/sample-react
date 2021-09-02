371af48f26349a6aabc876da695ab564dbbc8dd6
Kevin Rattan <kevin@time2yak.com>
2021-09-02 08:13:03 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import EventList from './components/events/EventList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="jumbotron">   
+          <h1>Welcome to [Team Name's] React Client</h1>
       </header>
+      <EventList />
     </div>
   );
 }
---
+++ src/components/events/ApplicationEvent.js
@@ -0,0 +1,9 @@
+const ApplicationEvent = ({title, description, id}) => (
+        <div>
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <br />
+        </div>
+)
+
+export default ApplicationEvent;
